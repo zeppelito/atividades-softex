60906e81436ecc733141b10fd34c722ab89bd2ca
zeppelito <109116903+zeppelito@users.noreply.github.com>
2022-11-02 19:01:17 -0300

    Add files via upload

---
+++ petshop.py
@@ -0,0 +1,9 @@
+
+nome = input ("digite a nome do animal")
+raca = input ("digite a raça do animal")
+idade = input ("digite a idade do animal") 
+rnome = input ("digite o nome do responsável")
+fone = input ("digite o telefone do responsável")
+
+
+print('animal: {}\nnome: {}\nraca: {}\nidade: {}\nrnome: {}\nfone: {} ' .format(nome, raca, idade, rnome, fone))
